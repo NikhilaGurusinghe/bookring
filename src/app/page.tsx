f474dcd4ac20f6ff391a65e4510b41f844df5ef4
Nikhila Gurusinghe <u7300256@anu.edu.au>
2026-07-18 21:11:26 +1000

    Adding suspense around async client component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 import members from "./webring/membership-lists/default";
 import WebringAutoNavigate from "./webring/components/webring-auto-navigate";
 import Link from "next/link";
+import { Suspense } from "react";
 
 export default function Home() {
   return (
       <main className="font-bold">
-        <WebringAutoNavigate membersJSON={JSON.stringify(members)} />
+        <Suspense>
+          <WebringAutoNavigate membersJSON={JSON.stringify(members)} />
+        </Suspense>
       </main>
   );
 }
