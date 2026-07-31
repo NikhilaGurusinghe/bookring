4bd9e10500fe74c784b328303d360d3208ea2fcc
Nikhila Gurusinghe <u7300256@anu.edu.au>
2026-07-31 21:36:56 +1000

    Removing debug stuff on homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,18 +3,11 @@ import WebringScripts from "./webring/components/webring-scripts";
 
 export default function Home() {
   return (
-      <main className="font-bold">
+      <main>
         <Suspense>
           <WebringScripts />
         </Suspense>
-        <a href="/?action=next">go next test</a>
-        <br/>
-        <a href="/?action=previous">go previous test</a>
-        <br/>
-        <a href="/?action=random">go random test</a>
-        <br />
-        <a href="/?image=current">go to current image</a>
-        <div>Next.js test</div>
+        <div className="font-bold">Bookring</div>
       </main>
   );
 }
