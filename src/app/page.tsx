af9c04228f29cb14f71dad23c7f55aa163a1d2be
Nikhila Gurusinghe <u7300256@anu.edu.au>
2026-08-01 11:43:37 +1000

    centering a div 🤯

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,14 +4,16 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-      <main>
-        <Suspense>
-          <WebringScripts />
-        </Suspense>
+    <>
+      <Suspense>
+        <WebringScripts />
+      </Suspense>
+      <main className="flex flex-row min-h-screen justify-center items-center">
         <div className="font-bold inline-flex items-center gap-3">
           <Image src="https://nikhilagurusinghe.github.io/bookring/images/webring_widget/book.png" width={48} height={48} alt="Bookring Logo"/>
           <span className="pt-2">Bookring</span>
         </div>
       </main>
+    </>
   );
 }
